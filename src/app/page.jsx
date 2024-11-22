566d04ad79a9aa549e0811bd485772cb853872a0
Stefano <stefanocumbo72@gmail.com>
2024-11-22 20:29:32 +1100

    2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,34 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div>Hello World</div>;
+  return (
+  <div className=" h-full flex flex-col lg:flex-row sm:px-8 md:px-12 lg:px-20 xl:px-48">
+
+    {/* IMAGE CONTAINER */}
+    <div className="h-1/2 relative lg:h-full lg:w-1/2">
+      <Image src="/hero.png" alt ="" fill className="object-contain"></Image>
+
+    </div>
+    {/* TEXT CONTAINER */}
+    <div className="h-1/2   lg:h-full lg:w-1/2   flex flex-col gap-8 justify-center items-center ">
+      <h1 className="text-4xl font-bold md:text-5xl ">
+        Crafting Digital Experiences, Designing Tomorrow
+      </h1>
+      <p className="md:text-xl">
+        Welcome to my digital canvas, where innovation and creativity converge. With a keen eye for aesthetics and a mastery of code, my 
+        portfolio showcases a diverse collection of projects that reflect my commitment to excellence
+      </p>
+      <div className="flex gap-4 w-full">
+        <button className="p-4 rounded-xl ring-1 ring-black bg-black text-white"> View my work</button>
+        <button className="p-4 rounded-xl ring-1 ring-black"> Contact me</button>
+      </div>
+      
+
+    </div>
+
+  </div>)
+ 
+  
 };
 
 export default Homepage;
