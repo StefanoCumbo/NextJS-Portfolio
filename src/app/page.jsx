5d84e91dddab8980be2957e6a740e64b54101698
Stefano <stefanocumbo72@gmail.com>
2024-12-03 23:00:30 +1100

    changed for xs screens

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,17 +4,17 @@ import {motion} from "framer-motion"
 import Link from "next/link";
 const Homepage = () => {
   return (
-  <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}}  transition={{duration:1.5 }}>
+  <motion.div className="h-full overflow-scroll" initial={{y:"-200vh"}} animate={{y:"0%"}}  transition={{duration:1.5 }}>
     <div className=" h-full flex flex-col lg:flex-row sm:px-8 md:px-12 lg:px-20 xl:px-48">
 
     {/* IMAGE CONTAINER */}
-    <div className="h-1/2 relative lg:h-full lg:w-1/2">
+    <div className="h-1/2 relative  lg:h-full lg:w-1/2">
       <Image src="/hero.png" alt ="" fill className="object-contain"></Image>
 
     </div>
     {/* TEXT CONTAINER */}
     <div className="h-1/2   lg:h-full lg:w-1/2   flex flex-col gap-8 justify-center items-center ">
-      <h1 className="text-4xl font-bold md:text-5xl ">
+      <h1 className=" xs:text-lg  text-4xl font-bold md:text-5xl   ">
         Crafting Digital Experiences, Designing Tomorrow
       </h1>
       <p className="md:text-xl">
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        xs: '300px', // Custom breakpoint for smaller screens
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
